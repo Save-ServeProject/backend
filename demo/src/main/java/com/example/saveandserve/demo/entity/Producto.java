ddc607b3a0f794ea8f2dbec06c4f94fbfc874dfc
Belen de la Moneda Rico <carlos.bertomeu@iesdoctorbalmis.com>
2025-01-24 12:42:42 +0100

    tabla producto agregada

--- demo/src/main/java/com/example/saveandserve/demo/entity/Producto.java
+++ demo/src/main/java/com/example/saveandserve/demo/entity/Producto.java
@@ -1,5 +1,37 @@
 package com.example.saveandserve.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "productos") 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Producto {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String idProducto;
+
+    @Column(nullable = false)
+    private String precioUnitario; 
+
+    public enum TipoProducto {
+        SECO,
+        REFRIGERADO,
+        CONGELADO
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoProducto tipoProducto; 
 }
