699e51929ad0080622bb84eac9a1e725c2c61565
Belen de la Moneda Rico <carlos.bertomeu@iesdoctorbalmis.com>
2025-01-28 12:14:43 +0100

    API-producto

--- demo/src/main/java/com/example/saveandserve/demo/repository/ProductoRepository.java
+++ demo/src/main/java/com/example/saveandserve/demo/repository/ProductoRepository.java
@@ -2,7 +2,8 @@ package com.example.saveandserve.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.saveandserve.demo.entity.Producto;
+import java.util.Optional;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
+    Optional<Producto> findByIdProducto(String idProducto);
 }
