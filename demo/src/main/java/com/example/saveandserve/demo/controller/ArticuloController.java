27c1170da59b1999f847ea71a9ee7dd3d208a6ba
Belen de la Moneda Rico <belen.delamoneda@iesdoctorbalmis.com>
2025-02-04 11:27:12 +0100

    funcionalidad - edicion de articulos

--- demo/src/main/java/com/example/saveandserve/demo/controller/ArticuloController.java
+++ demo/src/main/java/com/example/saveandserve/demo/controller/ArticuloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +47,21 @@ public class ArticuloController {
         articuloService.deleteArticulo(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Articulo> updateArticulo(@PathVariable Long id, @RequestBody Articulo articuloActualizado) {
+    Optional<Articulo> articuloOptional = articuloService.getArticuloById(id);
+    if (articuloOptional.isPresent()) {
+        Articulo articuloExistente = articuloOptional.get();
+        articuloExistente.setTitulo(articuloActualizado.getTitulo());
+        articuloExistente.setSubtitulo(articuloActualizado.getSubtitulo());
+        articuloExistente.setImagen(articuloActualizado.getImagen());
+        articuloExistente.setContenido(articuloActualizado.getContenido());
+
+        return ResponseEntity.ok(articuloService.saveArticulo(articuloExistente));
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
+
 }
