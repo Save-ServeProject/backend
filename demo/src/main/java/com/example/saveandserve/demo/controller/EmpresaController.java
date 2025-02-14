17768779a53bf83ac85fd5995291df0ce1eab1da
Cristina Ruiz <cristina.ruiz@iesdoctorbalmis.com>
2025-02-14 09:41:54 +0100

    holi

--- demo/src/main/java/com/example/saveandserve/demo/controller/EmpresaController.java
+++ demo/src/main/java/com/example/saveandserve/demo/controller/EmpresaController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,11 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.saveandserve.demo.entity.Empresa;
 import com.example.saveandserve.demo.service.EmpresaService;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 
 @RestController
 @RequestMapping("/empresas")
--- demo/src/main/java/com/example/saveandserve/demo/service/EmpresaService.java
+++ demo/src/main/java/com/example/saveandserve/demo/service/EmpresaService.java
@@ -1,11 +1,10 @@
 package com.example.saveandserve.demo.service;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
